f2eef0eefa43997eb60fb220065682727edacf64
Felipe Buccioni <falcacibar@gmail.com>
2022-10-03 12:41:51 -0300

    Improve: SQL databases

--- app/models/db/example_sql.py
+++ app/models/db/example_sql.py
@@ -5,7 +5,7 @@ from sqlmodel import SQLModel, Field
 from .. import example
 
 
-class Example(example.Example, SQLModel):
-    __tablename__: str = "example"
-
+class Example(example.Example, SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+    __tablename__: str = "example"
--- app/util/sql/__init__.py
+++ app/util/sql/__init__.py
@@ -1,5 +1,11 @@
+from typing import Type, Any
+
+from fastapi import HTTPException
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
 from ...sql import engine
 
@@ -13,3 +19,50 @@ async def get_session() -> AsyncSession:
     )
     async with async_session() as session:
         yield session
+
+
+async def get_or_404(db: AsyncSession, model: Type[SQLModel], where: Any) -> SQLModel:
+    instance: SQLModel = (
+        await db.execute(
+            select(model).where(where)
+        )
+    )\
+        .first()
+
+    if not instance:
+        raise HTTPException(404)
+
+    return instance
+
+
+async def create(db, model: Type[BaseModel], sql_model: Type[SQLModel]):
+    db_row = sql_model(**model.dict(skip_defaults=True))
+    db.add(db_row)
+
+    await db.commit()
+    await db.update(db_row)
+
+    return db_row
+
+
+async def partial_update(
+    db: AsyncSession, model: Type[BaseModel], sql_model: Type[SQLModel], where: Any
+):
+    db_row = (
+        await db.execute(
+            select(sql_model).where(where)
+        )
+    )\
+        .first()
+
+    if not db_row:
+        raise HTTPException(404)
+
+    for prop, value in model.dict(skip_defaults=True).items():
+        setattr(db_row, prop, value)
+
+    await db.add(db_row)
+    await db.commit()
+    await db.update(db_row)
+
+    return db_row
